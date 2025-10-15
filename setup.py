7e0af2a63a2e625022a5b71c49faed3b2caccef6
“Naveenpanguluri1018” <Naveenpanguluri1018.github.ie>
2025-10-15 11:26:59 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from pkg_resources import Requirement
+from setuptools import setup, find_packages
+from typing import List
+hypen_e_dot = '-e .'
+
+def get_requirements(file_path:str)->list[str]:
+    """This function will return the list of requirements"""
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+if hypen_e_dot in requirements:
+        requirements.remove(hypen_e_dot)   
+    return requirements
+
+setup(    name="mlproject",
+    version="0.1.0",
+    author="Naveenpanguluri1018",
+    author_email="naveenpanguluri1018.github.io",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
